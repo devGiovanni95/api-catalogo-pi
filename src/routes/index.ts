8fca4b0a527e661368af3066dc7d6146c2bf029d
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-10-01 00:53:00 -0300

    1.6.0-Beta | Implemented middleware verification with authetication jwt for the routes access

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import categoryRoutes from "./category/category.routes";
 import productRoutes from "./product/product.routes";
 import userRoutes from "./user/user.routes";
 import authRoutes from './authentication/authenticatio.routes';
+import AuthMiddLeware from "../middlewares/auth.middleware";
 
 const routes = Router();
 
-routes.use('/category', categoryRoutes)
-routes.use('/product', productRoutes)
-routes.use('/user', userRoutes)
+routes.use('/category', AuthMiddLeware, categoryRoutes)
+routes.use('/product', AuthMiddLeware, productRoutes)
+routes.use('/user', AuthMiddLeware, userRoutes)
 routes.use('/auth', authRoutes)
 
 export default routes
