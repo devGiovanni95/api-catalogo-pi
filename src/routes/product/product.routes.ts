633587a9313e599b1bbc96b92ef50cb3d50a3c04
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-11-05 19:50:00 -0300

    3.0.2 | Test api without middleware

--- src/routes/product/product.routes.ts
+++ src/routes/product/product.routes.ts
@@ -3,11 +3,11 @@ import ProductController from '../../controllers/product/product.controller'
 import AuthMiddLeware from '../../middlewares/auth.middleware'
 
 const productRoutes = Router()
-productRoutes.post('/', AuthMiddLeware, ProductController.store)
+productRoutes.post('/', /*AuthMiddLeware,*/ ProductController.store)
 productRoutes.get('/', ProductController.findAll)
 productRoutes.get('/:id', ProductController.findById)
 productRoutes.get('/category/:category', ProductController.findByCategory)
-productRoutes.delete('/:id', AuthMiddLeware, ProductController.delete)
-productRoutes.put('/:id', AuthMiddLeware, ProductController.update)
+productRoutes.delete('/:id', /*AuthMiddLeware,*/ ProductController.delete)
+productRoutes.put('/:id', /*AuthMiddLeware,*/ ProductController.update)
 
 export default productRoutes
